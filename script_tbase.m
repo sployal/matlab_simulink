058e4d0027b177bea307fd5c88578b7fa980938f
Davie_m <muigaid91@gmail.com>
2025-01-19 11:01:49 +0300

    Initial commit

---
+++ script_tbase.m
@@ -0,0 +1,7 @@
+
+
+y1 = sin(x);
+y2 = cos(x);
+
+figure
+plot(y1,y2);
---
+++ syspension_system_dim.m
@@ -0,0 +1,8 @@
+%% chassis dimensions 
+chassis_w = 30; % cm
+chassis_l = 60; %cm 
+chassis_h = 40; %cm
+
+%% syspension arm dimensions 
+%Arm 1 (upper arm)
+arm1_chassis_dist = 10; % cm 
